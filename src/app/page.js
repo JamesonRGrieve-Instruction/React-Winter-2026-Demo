08587e2442fc2ead519a1f83807a28a8eabbebcb
James Grieve <jamesonrgrieve@gmail.com>
2026-02-25 19:19:47 -0700

    State including referencing previous state;

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
-function Heading() {
-  return (<h1 className='text-4xl font-bold'>Hello, world!</h1>)
-}
+import Counter from "./Counter";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        {[...Array(3)].map((_, index) => <Heading key={index} />)}
-
+        <Counter />
       </main>
     </div>
   );
