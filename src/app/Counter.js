2359f76ff33c968a144575dcbfcdaae5bdaa0235
James Grieve <jamesonrgrieve@gmail.com>
2026-03-04 19:19:12 -0700

    Effect hooks;

--- src/app/Counter.js
+++ src/app/Counter.js
@@ -1,15 +1,33 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Counter() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState([0]);
+
+    useEffect(() => {
+        // On the New State's Way In
+        console.log("On Component Mount and State Change of Deps:" + count);
+        return () => {
+            // On the Old State's Way Out
+            console.log("On Component Unmount and Immediately Prior to State Change of Deps" + count);
+        }
+    }, [
+        count
+    ]);
+
+    useEffect(() => {
+        console.log("On Component Initial Mount (Once Per Mount)");
+        return () => {
+            console.log("On Component Final Unmount (Once Per Mount)");
+        }
+    }, [])
 
     return (
         <div>
             <p>{count}</p>
             <button onClick={() => {
-                setCount((previous) => previous + 1);
+                setCount((previous) => [...previous, previous[previous.length - 1] + 1]);
             }}>Count Up</button>
         </div>
     );
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
+"use client";
+import { useState } from "react";
 import Counter from "./Counter";
 
 export default function Home() {
+  const [showCounter, setShowCounter] = useState(true);
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Counter />
+        <button onClick={() => { setShowCounter(current => !current) }}>Toggle Counter</button>
+        {showCounter && <Counter />}
       </main>
     </div>
   );
