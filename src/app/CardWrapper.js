99657aea2f750e03618941fd0bf5dcd3530aa89b
James Grieve <jamesonrgrieve@gmail.com>
2026-03-11 19:28:28 -0600

    Open and close tagged / wrapper components;

---
+++ src/app/CardWrapper.js
@@ -0,0 +1,8 @@
+"use client";
+
+
+export default function CardWrapper({ children }) {
+    return <div className="border-2 rounded-md">
+        {children}
+    </div>
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 "use client"
 
 import { useCallback, useState } from "react";
+import CardWrapper from "./CardWrapper";
 import Counter from "./Counter";
-
 export default function Home() {
   const [pageCount, setPageCount] = useState(5);
 
@@ -15,6 +15,14 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1>The Count is {pageCount}</h1>
         <Counter start={pageCount} onChangeCallback={callbackPage} />
+        <CardWrapper>
+          <p>Hello, World.</p>
+        </CardWrapper>
+        <CardWrapper>
+          <ul>
+            <li>Item 1</li>
+          </ul>
+        </CardWrapper>
       </main>
     </div>
   );
