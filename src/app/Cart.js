8308930b7c947c4dd782eb4e2eabfb1cad258c7f
James Grieve <jamesonrgrieve@gmail.com>
2026-03-20 20:01:41 -0600

    Fix formatting;

--- src/app/Cart.js
+++ src/app/Cart.js
@@ -6,5 +6,9 @@ export default function Cart({ cartItems, setCartItems }) {
     const clearCart = useCallback(() => {
         setCartItems({})
     }, [setCartItems]);
-    return <><ul>{Object.entries(cartItems).map(item => <CartItem key={item[0]} name={item[0]} price={item[1].price} quantity={item[1].quantity} setCartItems={setCartItems} />)}</ul><p>Total: ${Object.values(cartItems).reduce((acc, curr) => acc + curr.price, 0)}</p><button onClick={clearCart}>Checkout Complete</button></>;
+    return <>
+        <ul>{Object.entries(cartItems).map(item => <CartItem key={item[0]} name={item[0]} price={item[1].price} quantity={item[1].quantity} setCartItems={setCartItems} />)}</ul>
+        <p>Total: ${Object.values(cartItems).reduce((acc, curr) => acc + curr.price, 0)}</p>
+        <button onClick={clearCart}>Checkout Complete</button>
+    </>;
 }
--- src/app/ProductList.js
+++ src/app/ProductList.js
@@ -3,6 +3,8 @@ import CartItem from "./CartItem";
 import Product from "./Product";
 export default function ProductList({ products, cartItems, setCartItems }) {
     return <ul>
-        {Object.entries(products).map(product => Object.keys(cartItems).includes(product[0]) ? <CartItem key={product[0]} name={product[0]} price={product[1]} quantity={cartItems[product[0]].quantity} setCartItems={setCartItems} /> : <Product key={product[0]} name={product[0]} price={Number(product[1])} setCartItems={setCartItems} />)}
+        {Object.entries(products).map(product => Object.keys(cartItems).includes(product[0])
+            ? <CartItem key={product[0]} name={product[0]} price={product[1]} quantity={cartItems[product[0]].quantity} setCartItems={setCartItems} />
+            : <Product key={product[0]} name={product[0]} price={Number(product[1])} setCartItems={setCartItems} />)}
     </ul>
 }
